6a2e86a7c133b70389a080eb426140778e08dadf
MaksimYurau <2004softwaredeveloper@gmail.com>
2023-12-20 23:44:30 +0300

    Добавьте файлы проекта.

---
+++ CalculatorMFC/CalculatorMFC.h
@@ -0,0 +1,32 @@
+﻿
+// CalculatorMFC.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CCalculatorMFCApp:
+// Сведения о реализации этого класса: CalculatorMFC.cpp
+//
+
+class CCalculatorMFCApp : public CWinApp
+{
+public:
+	CCalculatorMFCApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCalculatorMFCApp theApp;
---
+++ CalculatorMFC/CalculatorMFCDlg.h
@@ -0,0 +1,40 @@
+﻿
+// CalculatorMFCDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CCalculatorMFCDlg
+class CCalculatorMFCDlg : public CDialogEx
+{
+// Создание
+public:
+	CCalculatorMFCDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CALCULATORMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	double number1;
+	double number2;
+	double result;
+	afx_msg void OnBnClickedButtonCalculate();
+	afx_msg void OnBnClickedButtonClear();
+};
